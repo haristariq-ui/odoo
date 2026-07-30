0aca95d345b11dcaebd11cc39210258ef4c321ef
Muhammad Haris <haristariq087@gmail.com>
2026-07-30 19:08:09 +0500

    add sale order category

---
+++ sale_order_category/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': 'Sale Order Category',
+    'version': '1.0',
+    'depends': ['base','sale'],
+    'data': [
+        'views/category_view.xml',
+             ],
+    'installable': True,
+    'application': False,
+}
